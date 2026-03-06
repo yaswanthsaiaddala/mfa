9b4b43c8090210a75ace9ca955d5a31e83e56f15
yaswanthsaiaddala <yaswanth.addala@gmail.com>
2026-03-06 13:45:12 +0530

    initial commit

---
+++ container/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+MarketingApp;
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hi There</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+}
